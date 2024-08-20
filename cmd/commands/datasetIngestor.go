c9b7b9ff020ebf00192f8d5250010c87b0bc1c8b
consolethinks <32138106+consolethinks@users.noreply.github.com>
2024-08-20 12:49:19 +0200

    Update datasetIngestor.go

--- cmd/commands/datasetIngestor.go
+++ cmd/commands/datasetIngestor.go
@@ -166,8 +166,7 @@ For Windows you need instead to specify -user username:password on the command l
 		user, accessGroups := authenticate(RealAuthenticator{}, client, APIServer, userpass, token)
 
 		/* TODO Add info about policy settings and that autoarchive will take place or not */
-		metaDataMap, metaSourceFolder, beamlineAccount, err := datasetIngestor.ReadAndCheckMetadata(client, APIServer, metadatafile, user, accessGroups)
-
+		metaDataMap, metaSourceFolder, beamlineAccount, err := datasetIngestor.CheckMetadata(client, APIServer, metadatafile, user, accessGroups)
 		if err != nil {
 			log.Fatal("Error in CheckMetadata function: ", err)
 		}
